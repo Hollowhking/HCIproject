7a40a2ed4e7a7c4f90d47f0a0a04d162eba97b2a
Hollowhking <4trower@gmail.com>
2024-11-27 21:38:37 -0500

    change to app

---
+++ lib/Item.dart
@@ -0,0 +1,15 @@
+class Item{
+  int itemid;
+  String itemname;
+  String itemimgurl;
+  double cost;
+
+  Item(this.itemid, this.itemname, this.itemimgurl, this.cost);
+}
+
+class Cart{
+  List<Item> items;
+  String date;
+
+  Cart(this.items, this.date);
+}
