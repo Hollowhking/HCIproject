fabb55b2e9368e24a5ce87af57cc885b58e07f12
= <nbarudi@outlook.com>
2024-11-28 12:30:02 -0500

    Format

--- lib/main.dart
+++ lib/main.dart
@@ -40,20 +40,51 @@ class MyApp extends StatelessWidget {
   }
 }
 
-Item coke = Item(001, "12pk Coca Cola", "https://i5.walmartimages.com/asr/f810b124-f8a0-4a93-82d2-3e770ea24345.361d24791354ad16ecc48cff4ceee35e.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF", 12.91);
-Item pringles = Item(002, "Pringles 500g", "https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/57dda498-e3a1-4944-b874-c58fff63365c/500x500.jpg", 4.99);
-Item chickenstrips = Item(003, "Janes Chicken Tenders", "http://i.bungo.ca/u/Es7SGJ.png", 12.91);
-Item cucumber = Item(004, "Cucumber", "https://i5.walmartimages.ca/images/Enlarge/840/154/6000205840154.jpg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF", 3.99);
-Item carrots = Item(005, "Carrots 50g", "https://i5.walmartimages.ca/images/Enlarge/328/479/6000207328479.jpg", 2.50);
-Item Oreo = Item(006, "Oreo cookies 495g", "https://i5.walmartimages.com/asr/db7d72d9-68da-4ee2-9838-ea45824ff7af.3327e3c6c5731b6a6190eba573c8a8f9.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF", 3.45);
-Item dietcoke = Item(007, "12pk Diet Coca Cola", "http://i.bungo.ca/u/FIsdyL.png", 12.91);
+Item coke = Item(
+    001,
+    "12pk Coca Cola",
+    "https://i5.walmartimages.com/asr/f810b124-f8a0-4a93-82d2-3e770ea24345.361d24791354ad16ecc48cff4ceee35e.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF",
+    12.91);
+Item pringles = Item(
+    002,
+    "Pringles 500g",
+    "https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/57dda498-e3a1-4944-b874-c58fff63365c/500x500.jpg",
+    4.99);
+Item chickenstrips =
+    Item(003, "Janes Chicken Tenders", "http://i.bungo.ca/u/Es7SGJ.png", 12.91);
+Item cucumber = Item(
+    004,
+    "Cucumber",
+    "https://i5.walmartimages.ca/images/Enlarge/840/154/6000205840154.jpg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF",
+    3.99);
+Item carrots = Item(
+    005,
+    "Carrots 50g",
+    "https://i5.walmartimages.ca/images/Enlarge/328/479/6000207328479.jpg",
+    2.50);
+Item oreo = Item(
+    006,
+    "Oreo cookies 495g",
+    "https://i5.walmartimages.com/asr/db7d72d9-68da-4ee2-9838-ea45824ff7af.3327e3c6c5731b6a6190eba573c8a8f9.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF",
+    3.45);
+Item dietcoke =
+    Item(007, "12pk Diet Coca Cola", "http://i.bungo.ca/u/FIsdyL.png", 12.91);
 
-
-List<Item> items = [coke, pringles, chickenstrips];
+List<Item> items = [
+  coke,
+  pringles,
+  chickenstrips,
+  cucumber,
+  carrots,
+  oreo,
+  dietcoke
+];
 
 Cart cart1 = Cart([coke, pringles], "2024-11-28");
-Cart cart2 = Cart([carrots,chickenstrips,Oreo,cucumber], "2024-11-03");
-Cart cart3 = Cart([coke,pringles, chickenstrips, cucumber, carrots, Oreo, dietcoke], "2024-10-20");
+Cart cart2 = Cart([carrots, chickenstrips, oreo, cucumber], "2024-11-03");
+Cart cart3 = Cart(
+    [coke, pringles, chickenstrips, cucumber, carrots, oreo, dietcoke],
+    "2024-10-20");
 
 User user1 = User([cart1, cart2, cart3], "Josh");
 
@@ -75,20 +106,23 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
         actions: [
-          IconButton(onPressed: () async{
-            //Go to cart history
-            final result = await Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => _CartHistoryPage(user1))
-            );
-          }, icon: const Icon(Icons.history)),
           IconButton(
-              onPressed: () async{
+              onPressed: () async {
+                //Go to cart history
                 final result = await Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => _settingspage(user1))
-                );
-              }, icon: const Icon(Icons.settings))
+                    MaterialPageRoute(
+                        builder: (context) => _CartHistoryPage(user1)));
+              },
+              icon: const Icon(Icons.history)),
+          IconButton(
+              onPressed: () async {
+                final result = await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => _settingspage(user1)));
+              },
+              icon: const Icon(Icons.settings))
         ],
       ),
       body: Center(
@@ -158,10 +192,11 @@ class _ScannerPageState extends State<_ScannerPage> {
                     setState(() {});
 
                     await Future.delayed(const Duration(seconds: 3));
-                    final result = await await _addItemToCartDialog(activeItem!);
+                    final result =
+                        await await _addItemToCartDialog(activeItem!);
 
-                    if(result != null && result) {
-                      cart1.additem(activeItem!);
+                    if (result != null && result) {
+                      currentCart.additem(activeItem!);
                     }
 
                     activeItem = null;
@@ -220,7 +255,6 @@ class _ScannerPageState extends State<_ScannerPage> {
         ));
   }
 
-
   Future<Future<bool?>> _addItemToCartDialog(Item item) async {
     return showDialog<bool>(
       context: context,
@@ -232,21 +266,25 @@ class _ScannerPageState extends State<_ScannerPage> {
             child: Row(
               children: [
                 SizedBox(
-                  width: MediaQuery.of(context).size.width*0.3,
+                  width: MediaQuery.of(context).size.width * 0.3,
                   child: Image.network(item.itemimgurl),
                 ),
                 const Spacer(),
                 SizedBox(
-                  width: MediaQuery.of(context).size.width*0.3,
+                  width: MediaQuery.of(context).size.width * 0.3,
                   child: Column(
                     children: [
                       Text(
                         item.itemname,
-                        style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
                         textAlign: TextAlign.center,
                         softWrap: true,
                       ),
-                      Text("Price: ${item.cost}", style: const TextStyle(fontSize: 20),)
+                      Text(
+                        "Price: ${item.cost}",
+                        style: const TextStyle(fontSize: 20),
+                      )
                     ],
                   ),
                 )
@@ -255,7 +293,7 @@ class _ScannerPageState extends State<_ScannerPage> {
           ),
           actions: <Widget>[
             SizedBox(
-              width: MediaQuery.of(context).size.width*0.9,
+              width: MediaQuery.of(context).size.width * 0.9,
               child: Row(
                 children: [
                   TextButton(
@@ -269,8 +307,12 @@ class _ScannerPageState extends State<_ScannerPage> {
                     onPressed: () {
                       Navigator.of(context).pop(true);
                     },
-                    style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                    child: const Text('Add to Cart', style: TextStyle(color: Colors.white),),
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                    child: const Text(
+                      'Add to Cart',
+                      style: TextStyle(color: Colors.white),
+                    ),
                   )
                 ],
               ),
@@ -282,7 +324,7 @@ class _ScannerPageState extends State<_ScannerPage> {
   }
 }
 
-class _CartHistoryPage extends StatelessWidget{
+class _CartHistoryPage extends StatelessWidget {
   User user;
   _CartHistoryPage(this.user);
 
@@ -290,37 +332,37 @@ class _CartHistoryPage extends StatelessWidget{
   Widget build(BuildContext context) {
     print(user.history.length);
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Cart History"),
-      ),
-      body:ListView.separated(
-            itemCount: user.history.length,
-            itemBuilder: (BuildContext context, int index){
-              return GestureDetector(
-                onTap: () async{
-                  final result = await Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => _cartpageview(user.history[index]))
-                  );
-                },
-                child: Container(
-                  child: ListTile(
-                    shape: RoundedRectangleBorder(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text("Cart History"),
+        ),
+        body: ListView.separated(
+          itemCount: user.history.length,
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: () async {
+                final result = await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            _cartpageview(user.history[index])));
+              },
+              child: Container(
+                child: ListTile(
+                  shape: RoundedRectangleBorder(
                       side: BorderSide(color: Colors.grey, width: 1),
-                      borderRadius: BorderRadius.circular(5)
-                    ),
-                    title: Text(user.history[index].date),
-                    trailing: Text("${(user.history[index].gettotalwithtax()).toStringAsFixed(2)}"),
-                  ),
+                      borderRadius: BorderRadius.circular(5)),
+                  title: Text(user.history[index].date),
+                  trailing: Text(
+                      "${(user.history[index].gettotalwithtax()).toStringAsFixed(2)}"),
                 ),
-              );
-            },
-            separatorBuilder: (BuildContext context, int index) => const Divider(),
-          )
-    );
+              ),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) =>
+              const Divider(),
+        ));
   }
-
 }
 
 class _cartpageview extends StatelessWidget {
@@ -347,7 +389,8 @@ class _cartpageview extends StatelessWidget {
                   trailing: Text("${cart.items[index].cost}"),
                 );
               },
-              separatorBuilder: (BuildContext context, int index) => const Divider(),
+              separatorBuilder: (BuildContext context, int index) =>
+                  const Divider(),
             ),
           ),
           BottomAppBar(
@@ -356,8 +399,9 @@ class _cartpageview extends StatelessWidget {
                 Spacer(),
                 Column(
                   children: [
-                    Text("Tax: ${(cart.gettotal()*0.13).toStringAsFixed(2)}"),
-                    Text("Total: ${(cart.gettotal() + (cart.gettotal()*0.13)).toStringAsFixed(2)}"),
+                    Text("Tax: ${(cart.gettotal() * 0.13).toStringAsFixed(2)}"),
+                    Text(
+                        "Total: ${(cart.gettotal() + (cart.gettotal() * 0.13)).toStringAsFixed(2)}"),
                   ],
                 )
               ],
@@ -369,7 +413,7 @@ class _cartpageview extends StatelessWidget {
   }
 }
 
-class _settingspage extends StatelessWidget{
+class _settingspage extends StatelessWidget {
   User currentuser;
 
   _settingspage(this.currentuser);
@@ -383,5 +427,4 @@ class _settingspage extends StatelessWidget{
       ),
     );
   }
-
 }
